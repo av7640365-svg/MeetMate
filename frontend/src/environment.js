94d3950b33571f3891704f84984ab9d517b9833a
Anurag Verma <av7640365@gmai.com>
2026-08-10 22:54:12 +0530

    Update backend URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 
 let IS_PROD = true;
-const server = IS_PROD ? "https://meetmatebackend-52e6.onrender.com":"http:localhost:8000"
+const server = IS_PROD ? "https://meetmatebackend-52e6.onrender.com":"http://localhost:8000"
 
 
 export default server;
